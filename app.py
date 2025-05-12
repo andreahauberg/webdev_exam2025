d6d27a6605907630efa793d4c8bdac23e6c8f296
Andrea <132267442+andreahauberg@users.noreply.github.com>
2025-05-12 11:26:06 +0200

    hell

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "Hello Andrea"  # This should return "Hello Andrea"
 
 if __name__ == "__main__":
     app.run()
